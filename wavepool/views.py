75c0496136f9a235d503733bd64459d45d57c7d5
wbaxter3 <wbaxter3@gmail.com>
2021-02-25 19:56:04 -0500

    Revert to original

--- wavepool/views.py
+++ wavepool/views.py
@@ -7,33 +7,22 @@ from django.conf import settings
 
 
 def front_page(request):
-    """ View for the site's front page
+    """ View for the site's front age
         Returns all available newsposts, formatted like:
             cover_story: the newsposts with is_cover_story = True
             top_stories: the 3 most recent newsposts that are not cover story
             archive: the rest of the newsposts, sorted by most recent
     """
     template = loader.get_template('wavepool/frontpage.html')
-    # gets cover story
-    cover_story = NewsPost.objects.filter(is_cover_story=True)
-    # gets 3 most recent posts
-    top_stories = NewsPost.objects.filter(is_cover_story=False).order_by('-publish_date')[:3]
-    # gets rest of posts
-    other_stories = NewsPost.objects.filter(is_cover_story=False).order_by('-publish_date')[3:]
+    cover_story = NewsPost.objects.all().order_by('?').first()
+    top_stories = NewsPost.objects.all().order_by('?')[:3]
+    other_stories = NewsPost.objects.all().order_by('?')
 
-    # if there is a cover story pass the index 0, else pass empty query set
-    if len(cover_story) > 0:
-        context = {
-            'cover_story': cover_story[0],
-            'top_stories': top_stories,
-            'archive': other_stories,
-        }
-    else:
-        context = {
-            'cover_story': cover_story,
-            'top_stories': top_stories,
-            'archive': other_stories,
-        }
+    context = {
+        'cover_story': cover_story,
+        'top_stories': top_stories,
+        'archive': other_stories,
+    }
 
     return HttpResponse(template.render(context, request))
 
